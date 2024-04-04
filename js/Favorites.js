08322149b56e71548017f7a99851aab5fb70f759
Davi Teixeira <daviteixeira-btm@gmail.com>
2024-04-04 20:53:04 -0300

    [Davi] Creating HTML with JavaScript

--- js/Favorites.js
+++ js/Favorites.js
@@ -4,7 +4,19 @@ serão estruturados */
 export class Favorites {
     constructor(root){
         this.root = document.querySelector(root);
+        this.load();
     }
+
+    load(){
+        this.entries = [
+            {
+                login: "daviteixeira-btm",
+                name: "Davi Teixeira",
+                public_repos: "80",
+                followers: "101"
+            }
+        ];
+    };
 };
 
 /* Classe que vai criar a visualização e os eventos do HTML */
@@ -12,17 +24,62 @@ export class Favorites {
 export class FavoritesView extends Favorites {
     constructor(root){
         super(root);
+
+        this.tbody = this.root.querySelector("table tbody");
+
         this.update();
     };
 
     update(){
         this.removeAllTr();
+
+        this.entries.forEach(user => {
+            const row = this.createRow();
+
+            row.querySelector(".user img").src = `https://github.com/${user.login}.png`
+            
+            row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+            
+            row.querySelector(".user p").textContent = user.name;
+
+            row.querySelector(".user span").textContent = user.login;
+
+            row.querySelector(".repositories").textContent = user.public_repos;
+
+            row.querySelector(".followers").textContent = user.followers;
+
+            this.tbody.append(row);
+        });
     };
 
-    removeAllTr(){
-        const tbody = this.root.querySelector("table tbody");
+    createRow(){
 
-        tbody.querySelectorAll("tr")
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+        <td class="user">
+            <img src="https://github.com/daviteixeira-btm.png" alt="Imagem de daviteixeira-btm">
+            <a href="https://github.com/daviteixeira-btm" target="_blank">
+                <p>Davi Teixeira</p>
+                <span>daviteixeira-btm</span>
+            </a>
+        </td>
+        <td class="repositories">
+            80
+        </td>
+        <td class="followers">
+            101
+        </td>
+        <td>
+            <button class="remove">&times;</button>
+        </td>
+        `
+
+        return tr;
+    }
+
+    removeAllTr(){
+        this.tbody.querySelectorAll("tr")
             .forEach((tr) => {
                 tr.remove();
             });
