306948801db1d20a56001125a97bc2d586fb1c4e
Davi Teixeira <daviteixeira-btm@gmail.com>
2024-04-05 15:41:18 -0300

    [Davi] Some modifications to the code

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,16 +1,4 @@
-export class GithubUser {
-    static search(username){
-        const endpoint = `https://api.github.com/users/${username}`;
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(({ login, name, public_repos, followers }) => ({
-            login,
-            name,
-            public_repos,
-            followers
-        }));
-    };
-};
+import { GithubUser } from "./GithubUser.js";
 
 /* Classe que vai conter a lógica dos dados e como eles
 serão estruturados */
@@ -31,8 +19,14 @@ export class Favorites {
 
     async add(username){
         try {
+
+            const userExists = this.entries.find(entry => entry.login === username);
+
+            if(userExists){
+                throw new Error("Usuário já cadastrado!");
+            }
+
             const user = await GithubUser.search(username);
-            console.log(user);
 
             if(user.login === undefined){
                 throw new Error("Usuário não encontrado!");
@@ -89,6 +83,8 @@ export class FavoritesView extends Favorites {
             row.querySelector(".user img").src = `https://github.com/${user.login}.png`
             
             row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+            
+            row.querySelector(".user a").href = `https://github.com/${user.login}`;
             
             row.querySelector(".user p").textContent = user.name;
 
